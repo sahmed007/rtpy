46c989ffc74da1a043f70f77e9e275091a6f09c6
Samad Ahmed <samad@chamoylabs.com>
2024-08-24 22:00:08 -0500

    create a ppm image

---
+++ src/rtpy/main.py
@@ -0,0 +1,24 @@
+def main():
+    # Image
+
+    image_width = 256
+    image_height = 256
+
+    # Render
+
+    print(f"P3\n{image_width} {image_height}\n255")
+
+    for j in range(image_height):
+        for i in range(image_width):
+            r = i / (image_width - 1)
+            g = j / (image_height - 1)
+            b = 0.25
+
+            ir = int(255.999 * r)
+            ig = int(255.999 * g)
+            ib = int(255.999 * b)
+
+            print(f"{ir} {ig} {ib}")
+
+if __name__ == "__main__":
+    main()
